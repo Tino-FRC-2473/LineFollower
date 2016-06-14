55882cdf29d115f9e9fa062587534c31df964ca6
kashyappanda <kashyap.panda@gmail.com>
2016-06-13 20:27:08 -0700

    Added project

---
+++ FtcRobotController/src/main/java/com/qualcomm/ftcrobotcontroller/opmodes/LineFollower.java
@@ -0,0 +1,41 @@
+package com.qualcomm.ftcrobotcontroller.opmodes;
+
+import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
+import com.qualcomm.robotcore.eventloop.opmode.OpMode;
+import com.qualcomm.robotcore.hardware.ColorSensor;
+import com.qualcomm.robotcore.hardware.DcMotor;
+import com.qualcomm.robotcore.hardware.Servo;
+
+/**
+ * Created by Kashyap on 6/11/16.
+ */
+public class LineFollower extends LinearOpMode {
+    DcMotor A;
+    DcMotor B;
+    DcMotor C;
+    DcMotor D;
+    Servo L;
+    ColorSensor color;
+
+    @Override
+    public void runOpMode() throws InterruptedException {
+        hardwareMap.logDevices();
+        color = hardwareMap.colorSensor.get("CL");
+        A = hardwareMap.dcMotor.get("A");
+        B = hardwareMap.dcMotor.get("B");
+        C = hardwareMap.dcMotor.get("C");
+        D = hardwareMap.dcMotor.get("D");
+        L = hardwareMap.servo.get("L");
+
+        color.enableLed(true);
+
+        waitOneFullHardwareCycle();
+
+        waitForStart();
+
+        while (opModeIsActive()) {
+            int rgb[] = {color.red(), color.green(), color.blue()};
+            telemetry.addData("RGB ARRAY", rgb);
+        }
+    }
+}
