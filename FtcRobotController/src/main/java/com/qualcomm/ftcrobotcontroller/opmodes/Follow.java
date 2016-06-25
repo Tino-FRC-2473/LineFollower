174b9ea585510e414511c920b0b631b5b0e52b70
Deep Sethi <deeptanshu.146@gmail.com>
2016-06-24 21:09:58 -0700

    init method complete
    all values initialized, calibration complete

--- FtcRobotController/src/main/java/com/qualcomm/ftcrobotcontroller/opmodes/Follow.java
+++ FtcRobotController/src/main/java/com/qualcomm/ftcrobotcontroller/opmodes/Follow.java
@@ -10,18 +10,33 @@ import com.qualcomm.robotcore.hardware.GyroSensor;
  */
 public class Follow extends OpMode {
     /*Data creation: devices*/
-    DcMotor FR, FL, BR, BL;
-    AnalogInput L, C, R;
+    DcMotor fr, fl, br, bl;
+    AnalogInput l, c, r;
     GyroSensor spin;
 
     /*Data creation: other data*/
-    int analog_r, analog_c, analog_l; //sensor line values
+    int analog_r, analog_c, analog_l; //line values for each line sensor...setting a threshold
     double[] encoders; //encoder values for positional safekeeping
     double m = 1.0; //distance between each line sensor(they are equidistant)
 
     @Override
     public void init() {
+        fr = hardwareMap.dcMotor.get("FR");
+        fl = hardwareMap.dcMotor.get("FL");
+        br = hardwareMap.dcMotor.get("BR");
+        bl = hardwareMap.dcMotor.get("BL");
+        l = hardwareMap.analogInput.get("L");
+        c = hardwareMap.analogInput.get("C");
+        r = hardwareMap.analogInput.get("R");
+        spin = hardwareMap.gyroSensor.get("spin");
 
+        analog_r = r.getValue();
+        analog_c = c.getValue();
+        analog_l = l.getValue();
+
+        encoders = new double[2];
+        encoders[0] = 0.0;
+        encoders[1] = 0.0;
     }
 
     @Override
